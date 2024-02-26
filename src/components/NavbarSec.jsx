c0d4a1ecc3fc9d7e3a10a514ad23b197fa705534
Rahul A B <rahulbushi69@gmail.com>
2024-02-26 09:13:36 +0000

    Updated Navbar

--- src/components/NavbarSec.jsx
+++ src/components/NavbarSec.jsx
@@ -12,19 +12,14 @@ import { motion, AnimatePresence } from "framer-motion";
 
 function NavbarSec() {
     const navRef = useRef();
-
     const [Nav, SetNav] = useState(false);
-
-
     const handleNav = useCallback(() => {
         SetNav(!Nav);
     }, [Nav]);
-
     const variants = {
         open: { opacity: 1, y: 0, transition: { staggerChildren: 0.5 } },
         closed: { opacity: 0, y: "-100%" }
     };
-
     const itemVariants = {
         open: { opacity: 1, y: 0 },
         closed: { opacity: 0, y: -50 }
@@ -36,7 +31,6 @@ function NavbarSec() {
                 handleNav();
             }
         }
-        
         // Bind the event listener
         document.addEventListener("mousedown", handleClickOutside);
             return () => {
@@ -54,36 +48,46 @@ function NavbarSec() {
                 <div className="px-4 py-2 hidden sm:flex">
                     <NavLink to={'/'} className="text-2xl text-[#290c06] hover:text-[#fd9b40] transition duration-300 cursor-pointer">Home</NavLink>
                 </div>
-                <div to={'/'} className="flex flex-col items-center px-4 cursor-pointer">
+                <div className="hidden sm:flex flex-col items-center px-4 cursor-pointer">
                     <img src={Logo} alt="Floral Extracts" className="h-14" />
                     <h1 className="text-3xl text-[#290c06] font-bold ">Flora Extracts</h1>
-                    <div className="sm:hidden">
-                        {Nav ? <IoMdClose onClick={handleNav} className="text-4xl text-[#290c06] cursor-pointer absolute top-12 right-4" /> : <IoMenu onClick={handleNav} className="text-4xl text-[#290c06] cursor-pointer absolute top-12 right-4" />}
-                        <AnimatePresence>
-                            {Nav && (
-                                <motion.div ref={navRef} key="menu" initial="closed" animate="open" exit="closed" variants={variants} className="xl:hidden text-2xl w-40 absolute right-[110px] top-40 z-50 flex-col justify-center items-center bg-transparent text-center">
-                                    <motion.div onClick={handleNav} variants={itemVariants} className="bg-white border-[1px] border-[#33363F] py-2">
-                                        <NavLink to={'/'}>Home</NavLink>
-                                    </motion.div>
-                                    <motion.div onClick={handleNav} variants={itemVariants} className="bg-white border-[1px] border-[#33363F] py-2">
-                                        <NavLink to={'/shop'}>Products</NavLink>
-                                    </motion.div>
-                                    <motion.div onClick={handleNav} variants={itemVariants} className="bg-white border-[1px] border-[#33363F] py-2">
-                                        <NavLink to={'/about'}>About</NavLink>
-                                    </motion.div>
-                                    <motion.div onClick={handleNav} variants={itemVariants} className="bg-white border-[1px] border-[#33363F] py-2">
-                                        <NavLink to={'/contact'}>Contact</NavLink>
-                                    </motion.div>
-                                </motion.div>
-                            )}
-                        </AnimatePresence>
-                    </div>
                 </div>
                 <div className="px-4 py-2 hidden sm:flex">
                     <NavLink to={'/about'} className="text-2xl text-[#290c06] hover:text-[#fd9b40] transition duration-300 cursor-pointer">About</NavLink>
                 </div>
                 <div className="px-4 py-2 hidden sm:flex">
                     <NavLink to={'/contact'} className="text-2xl text-[#290c06] hover:text-[#fd9b40] transition duration-300 cursor-pointer">Contact</NavLink>
+                </div>
+            </div>
+            <div className="flex sm:hidden items-center justify-between px-5 relative">
+                <div className="flex items-center space-x-4 justify-center">
+                    <img src={Logo} alt="Floral Extracts" className="h-14" />
+                    <h1 className="text-3xl text-[#290c06] font-bold">Flora Extracts</h1>
+                </div>
+                <div className="">
+                    {Nav ? <IoMdClose onClick={handleNav} className="text-4xl text-[#290c06] cursor-pointer" /> : <IoMenu onClick={handleNav} className="text-4xl text-[#290c06] cursor-pointer" />}
+                </div>
+            </div>
+            <div className={`fixed left-0 w-full top-28 z-50 ${Nav ? '' : 'hidden'}`}>
+                <div className="w-full h-full flex items-center justify-center">
+                        <AnimatePresence>
+                            {Nav && (
+                                <motion.div ref={navRef} key="menu" initial="closed" animate="open" exit="closed" variants={variants} className="xl:hidden w-1/2 p-5 space-y-4 rounded-xl text-2xl flex-col items-center justify-center bg-white text-center z-30">
+                                    <motion.div onClick={handleNav} variants={itemVariants} className="bg-white border-[1px] border-[#33363F] py-2 rounded-xl">
+                                        <NavLink to={'/'}>Home</NavLink>
+                                    </motion.div>
+                                    <motion.div onClick={handleNav} variants={itemVariants} className="bg-white border-[1px] border-[#33363F] py-2 rounded-xl">
+                                        <NavLink to={'/shop'}>Products</NavLink>
+                                    </motion.div>
+                                    <motion.div onClick={handleNav} variants={itemVariants} className="bg-white border-[1px] border-[#33363F] py-2 rounded-xl">
+                                        <NavLink to={'/about'}>About</NavLink>
+                                    </motion.div>
+                                    <motion.div onClick={handleNav} variants={itemVariants} className="bg-white border-[1px] border-[#33363F] py-2 rounded-xl">
+                                        <NavLink to={'/contact'}>Contact</NavLink>
+                                    </motion.div>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                 </div>
             </div>
         </div>
