948520fcbe19c9b81ba098293322322097dc5757
Rab Lap <rahulbushi69@gmail.com>
2024-02-27 19:13:55 +0530

    Updated Shop/Natural Food Colour

--- src/sections/ShopSections/NaturalColors/NaturalColors.jsx
+++ src/sections/ShopSections/NaturalColors/NaturalColors.jsx
@@ -5,7 +5,7 @@ export default function NaturalColors() {
     return (
         <div className='flex flex-col items-center justify-center'>
             {/* Colours */}
-            <h1 className=' flex text-center justify-center font-bold text-4xl'>Colours</h1>
+            <h1 className=' flex text-center justify-center font-bold text-4xl py-5'>Natural Food Colours</h1>
             <NaturalColourTable />
         </div>
     );
