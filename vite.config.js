56a486c728d104c0445914261a96fafbe362b8f1
Rab Lap <rahulbushi69@gmail.com>
2024-04-26 22:34:39 +0530

    Updated: Contact Page

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'], // Add this line to handle JPG files
-  build: {
-    chunkSizeWarningLimit: 2000,
-  },
 });
