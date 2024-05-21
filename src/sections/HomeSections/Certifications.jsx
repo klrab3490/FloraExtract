c6320fd5de5d191fe4779f430fbc69d80e22b44d
Rab Lap <rahulbushi69@gmail.com>
2024-05-21 22:14:56 +0530

    Updated: Add New Certifications

--- src/sections/HomeSections/Certifications.jsx
+++ src/sections/HomeSections/Certifications.jsx
@@ -1,7 +1,16 @@
 // image
 import ISO from "../../assets/Certifications/iso-seeklogo.svg";
-import fasst from "../../assets/Certifications/Fssai Logo Vector.svg";
-import GDM from "../../assets/Certifications/GoodManufacturing.png";
+import FSSAI from "../../assets/Certifications/Fssai Logo Vector.svg";
+import GMP from "../../assets/Certifications/GoodManufacturing.png";
+import AEO from "../../assets/Certifications/AEO.png";
+import BRCGS from "../../assets/Certifications/BRCGS.png";
+import FAMI from "../../assets/Certifications/FAMI_QS.png";
+import FSSC22000 from "../../assets/Certifications/FSSC22000.png";
+import HALAL from "../../assets/Certifications/HALAL.png";
+import HALAL_INDO from "../../assets/Certifications/HALAL_INDO.png";
+import INDIA_ORGANIC from "../../assets/Certifications/INDIA_ORGANIC.png";
+import ORGANIC from "../../assets/Certifications/Organic_EU_Scope.png";
+import RA from "../../assets/Certifications/Rainforest_Alliance.png";
 
 function Certifications() {
     return (
@@ -16,10 +25,21 @@ function Certifications() {
                     Certifications
                 </div>
             </div>
-            <div className="flex justify-around items-center px-5 py-10">
-                <img src={ISO} alt="" className="sm:h-36 h-16"/>
-                <img src={fasst} alt="" className="sm:h-36 h-16"/>
-                <img src={GDM} alt="" className="sm:h-36 h-16" />
+            <div className="sm:px-10 px-4">
+                <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 px-5 py-10 place-items-center w-full sm:gap-2 gap-4">
+                    <img src={ISO} alt="ISO" className="sm:h-36 h-16 "/>
+                    <img src={FSSAI} alt="FASSAT" className="sm:h-36 h-16 w-44"/>
+                    <img src={GMP} alt="Good Manufacturing Practice" className="sm:h-36 h-16" />
+                    <img src={AEO} alt="AEO Indian Customs" className="sm:h-36 h-16" />
+                    <img src={BRCGS} alt="BRCGS Food Safety" className="sm:h-36 h-16" />
+                    <img src={FAMI} alt="FAMI QS" className="sm:h-36 h-16" />
+                    <img src={FSSC22000} alt="FSSC 22000" className="sm:h-36 h-16" />
+                    <img src={HALAL} alt="" className="sm:h-36 h-16" />
+                    <img src={HALAL_INDO} alt="" className="sm:h-36 h-16" />
+                    <img src={INDIA_ORGANIC} alt="" className="sm:h-36 h-16" />
+                    <img src={ORGANIC} alt="" className="sm:h-36 h-16" />
+                    <img src={RA} alt="Rainforest Alliance" className="sm:h-36 h-16" />
+                </div>
             </div>
         </div>
     );
