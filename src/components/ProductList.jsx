db27d09862d828715e832987b6615aea9c6b73c1
Rab Lap <rahulbushi69@gmail.com>
2024-08-11 19:08:50 +0530

    refactor: Update ProductList component to include image and item list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,19 +3,16 @@ import PropTypes from 'prop-types';
 export default function ProductList({ title, items, image }) {
 	return (
 		<div className='flex justify-center items-center p-5'>
-			<div className='bg-white shadow-md rounded-lg overflow-hidden p-4 md:p-6 lg:p-8 w-full border-[2px] border-black'>
+			<div className='bg-white shadow-md rounded-lg overflow-hidden p-4 md:p-6 lg:p-8 w-full h-full border-[2px] border-black'>
 				<div className='flex items-center justify-center space-x-4 pb-4'>
 					{image && (
 						<img src={image} alt={title} className='w-20 h-20 rounded-full object-cover'/>
 					)}
-					<h2 className='text-lg font-bold'> {title} </h2>
+					<h2 className='text-2xl font-bold text-[#413f91]'> {title} </h2>
 				</div>
-				<div className='flex flex-wrap gap-2 justify-center'>
+				<div className='flex flex-wrap gap-2 justify-center text-lg'>
 					{items.map((item, index) => (
-						<p key={index}>
-							{item}
-							{index < items.length - 1 && ','}
-						</p>
+						<li key={index}>{item}</li>
 					))}
 				</div>
 			</div>
